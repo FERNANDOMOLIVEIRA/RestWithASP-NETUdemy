41072815e4a12c37417ec401cbc69940701bcddc
Fernando Marques Oliveira <fernando.marques@outlook.com>
2018-09-09 18:43:20 -0300

    Implementing the Persons service

---
+++ RestWithASPNETUdemy 02 - Using Diferent Verbs/RestWithASPNETUdemy/Services/Implementattions/PersonServiceImplementattion.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using RestWithASPNETUdemy.Model;
+
+namespace RestWithASPNETUdemy.Services.Implementattions
+{
+    public class PersonServiceImplementattion : IPersonService
+    {
+        private volatile int count;
+
+        public Person Create(Person person)
+        {
+            return person;
+        }
+
+        public void Delete(long id)
+        {
+        }
+
+        public List<Person> FindAll()
+        {
+            List<Person> persons = new List<Person>();
+            for (int i = 0; i < 8; i++)
+            {
+                Person person = MockPerson(i);
+                persons.Add(person);
+            }
+
+            return persons;
+        }
+
+        private Person MockPerson(int id)
+        {
+            return new Person
+            {
+                Id = IncrementAndGet(),
+                FirstName = "Person Name",
+                LastName = "Person Lastname",
+                Address = "Some address",
+                Gender = "Male"
+            };
+        }
+
+        private long IncrementAndGet()
+        {
+            return Interlocked.Increment(ref count);
+        }
+
+        public Person FindById(long id)
+        {
+            return new Person {
+                Id = 1,
+                FirstName = "Fernando",
+                LastName = "FRDB",
+                Address = "Brasília - DF - Brasil",
+                Gender = "Male"
+            };
+        }
+
+        public Person Update(Person person)
+        {
+            return person;
+        }
+    }
+}
